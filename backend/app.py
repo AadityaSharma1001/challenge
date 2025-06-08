b0d2f644bba9e590125feed81280c88f1c91b3e2
AadityaSharma1001 <b23me1001@iitj.ac.in>
2025-06-08 14:59:00 +0530

    minor fix

--- backend/app.py
+++ backend/app.py
@@ -5,7 +5,7 @@ import os
 
 def create_app():
     app = Flask(__name__)
-    CORS(app, origins=["http://localhost:5173"]) #Can be dynamically set using environment variables
+    CORS(app, origins=["https://challenge-ten-chi.vercel.app/"]) #Can be dynamically set using environment variables
     app.register_blueprint(prime_bp)
     return app
 
